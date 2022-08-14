21cbff27e3150ff3bc01c66ea6ddac791210cb00
pocahontasllol <pocahontasllol@gmail.com>
2022-08-14 14:34:09 +0300

    optional parameters

---
+++ src/app/App.js
@@ -0,0 +1,26 @@
+import NavBar from "../components/navBar";
+import { Route, Switch } from "react-router-dom";
+import Dashboard from "../components/dashboard";
+import Login from "../components/login";
+import Home from "../components/home";
+import Posts from "../components/posts";
+import Stats from "../components/stats";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <h1>App</h1>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/dashboard/stats" component={Stats} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route
+          path="/posts/:postId?" component={Posts}
+        />
+      </Switch>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/posts.jsx
@@ -0,0 +1,14 @@
+import PostsList from "./postList";
+import Post from "./post";
+
+const Posts = ({ match }) => {
+  const posts = [
+    { id: 1, label: "post 1" },
+    { id: 2, label: "post 2" },
+    { id: 3, label: "post 3" },
+  ];
+  const postId = match.params.postId;
+  return <>{postId ? <Post posts={posts} id={postId}/> : <PostsList posts={posts}/>}</>;
+};
+
+export default Posts;
